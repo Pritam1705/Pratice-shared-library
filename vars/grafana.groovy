7e0bb4f97167a868a895266a247f302e9a1208a8
Pritam1705 <kondapratiwarpritam855@gmail.com>
2025-01-02 17:19:58 +0530

    Update grafana.groovy

--- vars/grafana.groovy
+++ vars/grafana.groovy
@@ -1,3 +1,11 @@
 def checkout() {
-    org.ansible.Checkout()
+   // Ensure that 'org' is properly defined and accessible here
+    def repoUrl = 'https://github.com/Siddharth2419/CICD.git'
+    def branch = 'main'
+    // Perform git checkout
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name: "*/${branch}"]],
+        userRemoteConfigs: [[url: repoUrl]]
+    ])
 }
